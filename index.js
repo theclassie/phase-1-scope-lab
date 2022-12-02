c2b30145794e5855da4363fc1b86a77f956bff26
Tristyn Broadhead <trisclassie@gmail.com>
2022-12-02 01:19:52 -0700

    commit tree ad201cbd59106cbb0009ffb153557f21b510b608

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+};
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'rob'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'doug'
+}
